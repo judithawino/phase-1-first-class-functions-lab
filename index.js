d25ba25fec479a934f1047b0ba5d953a495dd2b5
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-08 21:15:48 +0300

    First-Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    let newDrivers = drivers.slice(0,2);
+    return newDrivers;
+
+}
+
+returnFirstTwoDrivers()
+
+function returnLastTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    let newDrivers = drivers.slice(2,4);
+    return newDrivers;
+}
+returnLastTwoDrivers()
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier= function (FareMultiplier){
+    return function(value){
+        return FareMultiplier*value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, funct) {
+  return funct(arrayOfDrivers);
+} 
+
+
+    
+
+
+
